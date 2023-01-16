55d06687f6526162daf71e64e0216184a065ece7
Ben Murray <benmurray573@gmail.com>
2023-01-16 16:30:30 +0000

    setup file and wrote get request for getting a specified users data

--- src/server.ts
+++ src/server.ts
@@ -1,67 +1,37 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import filePath from "./filePath";
+import { Client } from "pg"
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
-
 dotenv.config();
 
 const PORT_NUMBER = process.env.PORT ?? 4000;
+const client = new Client(process.env.DATABASE_URL);
+client.connect();
 
-// API info page
-app.get("/", (req, res) => {
-  const pathToFile = filePath("../public/index.html");
-  res.sendFile(pathToFile);
-});
+interface user {
+  id: number;
+  username: string;
+  isFaculty: boolean;
+}
 
-// GET /items
-app.get("/items", (req, res) => {
-  const allSignatures = getAllDbItems();
-  res.status(200).json(allSignatures);
-});
-
-// POST /items
-app.post<{}, {}, DbItem>("/items", (req, res) => {
-  // to be rigorous, ought to handle non-conforming request bodies
-  // ... but omitting this as a simplification
-  const postData = req.body;
-  const createdSignature = addDbItem(postData);
-  res.status(201).json(createdSignature);
-});
-
-// GET /items/:id
-app.get<{ id: string }>("/items/:id", (req, res) => {
-  const matchingSignature = getDbItemById(parseInt(req.params.id));
-  if (matchingSignature === "not found") {
-    res.status(404).json(matchingSignature);
-  } else {
-    res.status(200).json(matchingSignature);
+app.get('/users', async (req, res) => {
+  try {
+    const username = req.body.username
+    const queryResponse = await client.query(`
+    SELECT * FROM users
+    WHERE username = $1`, [username])
+    const loggedInUser = queryResponse.rows[0]
+    res.status(200).json(loggedInUser)
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ message: "internal error" });
   }
-});
+})
 
-// DELETE /items/:id
-app.delete<{ id: string }>("/items/:id", (req, res) => {
-  const matchingSignature = getDbItemById(parseInt(req.params.id));
-  if (matchingSignature === "not found") {
-    res.status(404).json(matchingSignature);
-  } else {
-    res.status(200).json(matchingSignature);
-  }
-});
-
-// PATCH /items/:id
-app.patch<{ id: string }, {}, Partial<DbItem>>("/items/:id", (req, res) => {
-  const matchingSignature = updateDbItemById(parseInt(req.params.id), req.body);
-  if (matchingSignature === "not found") {
-    res.status(404).json(matchingSignature);
-  } else {
-    res.status(200).json(matchingSignature);
-  }
-});
 
 app.listen(PORT_NUMBER, () => {
   console.log(`Server is listening on port ${PORT_NUMBER}!`);
