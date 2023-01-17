fdb61d9ff6d7bbf6eb2133db50a79cba81727fdc
Alessia B <borys.alessia@gmail.com>
2023-01-17 13:40:37 +0100

    resources get working

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { Client } from "pg"
+import { Client } from "pg";
 
 const app = express();
 app.use(express.json());
@@ -18,20 +18,53 @@ interface user {
   isFaculty: boolean;
 }
 
-app.get('/users', async (req, res) => {
+//------------------------------------------------gets all users
+app.get("/users", async (req, res) => {
   try {
-    const username = req.body.username
     const queryResponse = await client.query(`
-    SELECT * FROM users
-    WHERE username = $1`, [username])
-    const loggedInUser = queryResponse.rows[0]
-    res.status(200).json(loggedInUser)
+    SELECT * 
+    FROM users
+    `);
+    const allUsers = queryResponse.rows;
+    res.status(200).json(allUsers);
   } catch (error) {
     console.error(error);
-    res.status(404).json({ message: "internal error" });
+    res.status(404).json({ message: "error, get all users" });
   }
-})
+});
 
+//------------------------------------------------gets all information for specific user
+app.get("/users/:username", async (req, res) => {
+  try {
+    const username = req.params.username;
+    const queryResponse = await client.query(
+      `
+    SELECT * FROM users
+    WHERE username = $1`,
+      [username]
+    );
+    const loggedInUser = queryResponse.rows[0];
+    res.status(200).json(loggedInUser);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ message: "error, get one user" });
+  }
+});
+
+//------------------------------------------------gets all resources
+app.get("/resources", async (req, res) => {
+  try {
+    const queryResponse = await client.query(`
+    SELECT * 
+    FROM resources
+    `);
+    const allResources = queryResponse.rows;
+    res.status(200).json(allResources);
+  } catch (error) {
+    console.error(error);
+    res.status(404).json({ message: "error, get all resources" });
+  }
+});
 
 app.listen(PORT_NUMBER, () => {
   console.log(`Server is listening on port ${PORT_NUMBER}!`);
