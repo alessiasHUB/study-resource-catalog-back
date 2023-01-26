092645c28f169c883b53b865b5f3ed520269a886
Ben Murray <benmurray573@gmail.com>
2023-01-26 15:07:05 +0000

    get req for study list resources now working with new query

--- src/server.ts
+++ src/server.ts
@@ -124,16 +124,16 @@ app.get<{ userid: string }>("/study_list/:userid", async (req, res) => {
   }
 });
 //----------------------------------------------- get study list RESOURCES
-app.get("/study_resources/:resourceidArr", async (req, res) => { // => 4,5,2
+app.get("/study_resources/:userID", async (req, res) => { // => 4,5,2
   try {
-    console.log(req.params.resourceidArr)
-    const resourceIDArr = req.params.resourceidArr.split(",").map(Number); // => [4, 5, 2]
-    console.log(resourceIDArr)
+    console.log(req.params.userID)
     const queryResponse = await client.query(
-      `SELECT *
-       FROM resources
-       WHERE id IN ($1)`,
-       resourceIDArr
+      `SELECT r.id, r.user_id, r.title, r.link, r.description, r.tags, r.type, r.usage, r.post_date, r.likes, r.dislikes
+      FROM study_list s
+      JOIN resources r
+      ON s.resource_id = r.id
+      WHERE s.user_id = $1`,
+       [req.params.userID]
     );
     const allStudyListResources = queryResponse.rows;
     res.status(200).json(allStudyListResources);
